388ada13faeaef8cb5cb3b9bfaa68312576f178b
Ibrhime Qandeel <ibrahimgandeel@gmail.com>
2026-07-06 17:53:56 +0300

    Move Laravel project to repository root

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+    /**
+     * عرض الصفحة الرئيسية للموقع
+     */
     public function index()
     {
+        // تأكد أن ملف index.blade.php موجود داخل resources/views/ وبحروف صغيرة
         return view('index');
     }
 }
